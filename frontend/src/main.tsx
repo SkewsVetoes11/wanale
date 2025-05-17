27e009bf952858acd9ef2158eead3f5c57bfb8b6
Ricky Lu <ricky_lu0@outlook.com>
2025-05-17 07:18:17 -0700

    MUI & vite upgrade

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import {createRoot} from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import './style.css'
-import App from './App'
+import App from './App.js'
 
 const container = document.getElementById('root')
 
@@ -9,6 +9,6 @@ const root = createRoot(container!)
 
 root.render(
     <React.StrictMode>
-        <App/>
+        <App />
     </React.StrictMode>
 )
